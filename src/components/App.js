7cffc954000ed66cb8e3a09d54068b5c921be4dc
linazoo <linazooper@gmail.com>
2018-03-17 17:21:43 -0500

    built Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h2>Non-Wedding</h2>
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <h2> Hello bi ch </h2> 
+  );
+
+}
+
+export default Header;
