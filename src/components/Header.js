23f4207b14f6cc4c0bf579f7d3d884f4b3d829e8
linazoo <linazooper@gmail.com>
2018-03-17 19:15:06 -0500

    created UserInfo component and then put it inside the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Logo from './Logo.js';
+import UserInfo from './UserInfo';
 
 const Header = (props) => {
   return (
     <div className="header">
       <Logo />
+      <UserInfo />
     </div>
   );
 
---
+++ src/components/UserInfo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserInfo = (props) => {
+  return (
+    <h2> User Info </h2>
+  );
+}
+
+export default UserInfo;
