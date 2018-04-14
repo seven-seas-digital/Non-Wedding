d213ecc4bc561b5f370872c69fa79d32d8ed60fa
johnlyden <johnlydennc@gmail.com>
2018-04-14 13:27:41 -0500

    working checklist navigation and transitions

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -3,16 +3,21 @@ import ChecklistItem from './ChecklistItem';
 import Slider from "react-slick";
 
 const checklistMap = {
-  'budget': 1,
-  'apparel': 2,
-  'reception': 3,
-  'ceremony': 4
+  'budget': 0,
+  'apparel': 1,
+  'reception': 2,
+  'ceremony': 3
 }
 
 export default class Checklist extends Component {
   constructor(props) {
     super(props);
+    this.updateSlider = this.updateSlider.bind(this);
   }
+
+  // componentDidMount() {
+  //   this.updateSlider()
+  // }
 
   // componentDidUpdate(prevProps, prevState, snapshot) {
   //   const checklistType = prevProps.checklistType;
@@ -20,7 +25,9 @@ export default class Checklist extends Component {
   //   this.slider.slickGoTo(slideId);
   // }
 
-
+  updateSlider(id) {
+    this.slider.slickGoTo(id);
+  }
 
   render() {
     const checklistType = this.props.checklistType;
@@ -32,43 +39,45 @@ export default class Checklist extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       dots: false,
-      slickGoTo: slideId,
-      // ref: (ref) => this.slider = ref,
-      // afterChange: () => alert('hey'),
-      initialSlide: slideId
+      initialSlide: 0
     };
-    // this.sliderWrapper.slider.slickGoTo(slideId)
-    // this.slider.slickGoTo(slideId);
+
+    if (this.slider) {
+
+      this.updateSlider(slideId);
+    }
 
     return (
-      <Slider {...settings}>
-        <div className="checklist-container">
-          <h2>Budget</h2>
-          <ChecklistItem />
-          <ChecklistItem />
-          <ChecklistItem />
-        </div>
+      <div class="fuck">
+        <Slider ref={slider => (this.slider = slider)} {...settings}>
+          <div className="checklist-container">
+            <h2>Budget</h2>
+            <ChecklistItem />
+            <ChecklistItem />
+            <ChecklistItem />
+          </div>
 
-        <div className="checklist-container">
-          <h2>Apparel</h2>
-          <ChecklistItem />
-          <ChecklistItem />
-          <ChecklistItem />
-        </div>
+          <div className="checklist-container">
+            <h2>Apparel</h2>
+            <ChecklistItem />
+            <ChecklistItem />
+            <ChecklistItem />
+          </div>
 
-        <div className="checklist-container">
-          <h2>Reception</h2>
-          <ChecklistItem />
-          <ChecklistItem />
-          <ChecklistItem />
-        </div>
-        <div className="checklist-container">
-          <h2>Da Party</h2>
-          <ChecklistItem />
-          <ChecklistItem />
-          <ChecklistItem />
-        </div>
-      </Slider>
+          <div className="checklist-container">
+            <h2>Reception</h2>
+            <ChecklistItem />
+            <ChecklistItem />
+            <ChecklistItem />
+          </div>
+          <div className="checklist-container">
+            <h2>Ceremony</h2>
+            <ChecklistItem />
+            <ChecklistItem />
+            <ChecklistItem />
+          </div>
+        </Slider>
+      </div>
     );
   }
 }
