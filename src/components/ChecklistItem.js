b0a43b75717b486b63f57ecf95fe7e794962f4e2
linazoo <linazooper@gmail.com>
2018-04-29 20:47:43 -0500

    created a class component for checkllistitem to handleclick - working with an alert as of now

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ChecklistItem = (props) => {
-  return (
-    <div className="checklist-item">
-      <div className="content-container">
-        <p className="item-name">Income</p>
-        <p className="total-spent">$10,000</p>
-        <p className="item-subtitle">10 transactions</p>
-        <p className="budget">of $12,000</p>
+class ChecklistItem extends Component {
+    handleClick() {
+    // we want to update the state to open modal 
+  }
+  render () {
+    return (
+      <div className="checklist-item">
+        <div className="content-container">
+          <p className="item-name">Income</p>
+          <p className="total-spent">$10,000</p>
+          <p className="item-subtitle">10 transactions</p>
+          <p className="budget">of $12,000</p>
+        </div>
+        <div className="more-button" onClick={ this.handleClick }></div>
       </div>
-      <div className="more-button"></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ChecklistItem;
